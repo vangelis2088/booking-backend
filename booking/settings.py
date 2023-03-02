1d20b2d61c77473ac08d71959dd5025e1e2f59dd
Evangelos Tsougiannis <evangelos.tsougiannis@cibos.gr>
2023-03-02 14:34:37 +0200

    changes to settings.py

--- booking/settings.py
+++ booking/settings.py
@@ -100,10 +100,13 @@ DATABASES = {
     'default': {
         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
         # 'ENGINE': 'django.db.backends.mysql' instead of the following.
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'booking_database',
-        'USER': 'booking',
-        'PASSWORD': 'booking',
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'booking_database',
+        #'USER': 'booking',
+        #'PASSWORD': 'booking',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_USER'),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
