3fab24c0b6f49a16542df213cf9156bdb540b052
Evangelos Tsougiannis <evangelos.tsougiannis@cibos.gr>
2023-03-03 16:01:29 +0200

    Changed authentication method

--- booking/settings.py
+++ booking/settings.py
@@ -161,7 +161,7 @@ MEDIA_ROOT = 'uploads'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
